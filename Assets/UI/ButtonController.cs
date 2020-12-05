39d0b1dee630a0250fbffcc3576d77ae816b22e5
Sheldon <SheldonS.Larsen@gmail.com>
2020-12-05 06:54:59 -0700

    Updated Menu

---
+++ Assets/UI/ButtonController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ButtonController : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    public Text text;
+    public Color baseColor;
+    public Color hoverColor;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        text.color = hoverColor;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        text.color = baseColor;
+    }
+}
