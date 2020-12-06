1f6a6c49e002716c9302e173f87f496f80260268
Josh Nelson <josh.nelson1@yahoo.com>
2020-12-06 01:36:00 -0700

    Added several varieties of textures for zombies. Added a weighted random distributor for random textures.

---
+++ Assets/Scripts/MaterialTextureRandomizer.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialTextureRandomizer : MonoBehaviour
+{
+    static Dictionary<int,int>weightedIndex;//oof singletons :( #GameJamProblems
+    static bool initialized = false;
+    static int size;
+
+    public SkinnedMeshRenderer rend;
+    [System.Serializable]
+   public struct WeightedTexture
+    {
+        [SerializeField]
+        public Texture2D tex;
+        [SerializeField]
+        public int chance;
+    }; 
+    public List<WeightedTexture> options;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(!initialized)
+        {
+            initDistribution();
+        }
+
+        SetRandomTexture();
+    }
+
+
+    void SetRandomTexture()
+    {
+        int sampleIndex = UnityEngine.Random.Range(0, size);
+        int index = weightedIndex[sampleIndex];
+
+        rend.material.SetTexture("_BaseMap", options[index].tex);
+    }
+
+    void initDistribution()
+    {
+        weightedIndex = new Dictionary<int, int>();
+        size = 0;
+        initialized = true;
+        int elementNum = 0;
+        foreach(WeightedTexture current in options)
+        {
+            for(int i=0;i<current.chance;i++)
+            {
+                weightedIndex.Add(size, elementNum);
+                size++;                
+            }
+            elementNum++;
+        }
+    }
+
+}
