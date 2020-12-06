d5f24b50c2d2e5a0da62d892743c0b87c63bc2ee
Sheldon <SheldonS.Larsen@gmail.com>
2020-12-06 00:40:25 -0700

    Added Death

---
+++ Assets/Scripts/DeathSystem/DeathScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathScript : MonoBehaviour
+{
+    public GameObject worldSpawn;
+    private GameObject lastCheckPoint;
+
+    private void Start()
+    {
+        if(worldSpawn == null)
+        {
+            GameObject notsetSpawnObject = new GameObject();
+            notsetSpawnObject.transform.position = this.gameObject.transform.position;
+            notsetSpawnObject.transform.rotation = this.gameObject.transform.rotation;
+            notsetSpawnObject.name = "AutoGenWorldSpawnPoint";
+            worldSpawn = notsetSpawnObject;
+        }
+    }
+
+    public void ResetPlayer()
+    {
+        if (lastCheckPoint != null)
+        {
+            GoToLocation(lastCheckPoint);
+        } else {
+            GoToLocation(worldSpawn);
+        }
+    }
+
+    public void KillPlayer()
+    {
+        GoToLocation(worldSpawn);
+    }
+
+    void GoToLocation(GameObject location)
+    {
+        this.transform.position = location.transform.position;
+        this.transform.rotation = location.transform.rotation;
+    }
+
+    public void SetCheckpoint(GameObject location)
+    {
+        lastCheckPoint = location;
+    }
+}
