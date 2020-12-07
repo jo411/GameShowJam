ae588656cd4b542300b8d7cd1b5175a068b57081
Sheldon <SheldonS.Larsen@gmail.com>
2020-12-07 04:32:46 -0700

    Added Tail

--- Assets/Scripts/DeathSystem/TailController.cs
+++ Assets/Scripts/DeathSystem/TailController.cs
@@ -2,20 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class TailController : MonoBehaviour
 {
-    public static bool hasTail = false;
-    public float FFLTime = 5f;
-
+    public bool hasTail = true;
+    public float FFLTime = 10f;
+    public float StealRadias = 5f;
+    public float TheftCheckCoolDown = 2f;
+    public AnimationCurve NumberOfZombiesToStealChance = AnimationCurve.Linear(0, 0, 50, 80);
+    
     private float FFLTimePassed = 0f;
-    private DeathScript deathScript;
+    private float TimeSinceLastTheft = 0f;
+
+
+    [SerializeReference]
+    private List<GameObject> Zombies = null;
+
+
+    private DeathScript deathScript = null;
+    private ZombieController ZombieWithTail = null;
 
     // Start is called before the first frame update
     void Start()
     {
-        hasTail = true;
-        AddTail();
         deathScript = this.GetComponent<DeathScript>() as DeathScript;
+        Random.InitState((int)System.DateTime.Now.Ticks);
     }
 
     // Update is called once per frame
@@ -28,7 +39,24 @@ public class TailController : MonoBehaviour
             if(FFLTimePassed >= FFLTime)
             {
                 deathScript.KillPlayer();
+                
+                if (ZombieWithTail != null)
+                {
+                    ZombieWithTail.RemoveTail();
+                    ZombieWithTail = null;
+                }
+
                 AddTail();
+            }
+        } else
+        {
+            TimeSinceLastTheft += Time.deltaTime;
+
+            if (TimeSinceLastTheft >= TheftCheckCoolDown)
+            {
+                GetCloseZombies();
+                CalculateTheft();
+                TimeSinceLastTheft = 0f;
             }
         }
     }
@@ -38,16 +66,50 @@ public class TailController : MonoBehaviour
         hasTail = true;
     }
 
-    public bool RemoveTail()
+    private void RemoveTail()
     {
-        if (hasTail)
+        hasTail = false;
+        FFLTimePassed = 0f;
+        AddTailToZombie();
+    }
+
+    private void GetCloseZombies()
+    {
+        Zombies.Clear();
+
+        Collider[] colliders = Physics.OverlapSphere(this.transform.position, StealRadias);
+
+        foreach (Collider col in colliders)
         {
-            hasTail = false;
-            FFLTimePassed = 0f;
-            return true;
-        } else
-        {
-            return false;
+            if(col.gameObject.tag == "zombie")
+            {
+                Zombies.Add(col.gameObject);
+            }
         }
+    }
+
+    private void CalculateTheft()
+    {
+        if(Zombies.Count > 0)
+        {
+           int chance = (int)NumberOfZombiesToStealChance.Evaluate(Zombies.Count);
+
+           if (checkPercentage(chance))
+           {
+                RemoveTail();
+           }
+        }
+    }
+
+    private bool checkPercentage(int chanceTrue)
+    {
+        return Random.Range(0, 101) <= chanceTrue;
+    }
+
+    private void AddTailToZombie()
+    {
+       GameObject zombie = Zombies[Random.Range(0, Zombies.Count)];
+       ZombieWithTail = zombie.GetComponent<ZombieController>() as ZombieController;
+       ZombieWithTail.AddTail();
     }
 }
