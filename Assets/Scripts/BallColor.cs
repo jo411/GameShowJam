6f98f1a3c23a3ce4295d901fe1faed2a0d7177e4
xcseage <xcseage@gmail.com>
2020-12-10 13:46:37 -0700

    Finished level

---
+++ Assets/Scripts/BallColor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallColor : MonoBehaviour
+{
+    public List<Color> colors;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Color Set");
+
+        Renderer color = GetComponent<Renderer>();
+        
+        color.material.color = colors[Random.Range(0, colors.Count)];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/FallGuy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallGuy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("zombie"))
+        {
+            other.gameObject.GetComponent<ZombieController>().DisableNavmeshAgentForSeconds();
+        }
+    }
+}
